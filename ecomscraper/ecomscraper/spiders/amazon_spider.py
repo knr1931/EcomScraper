4afa1a725e4fcff1c04d46f13b657a31da7823c4
Nitesh Reddy <7451291+knr1931@users.noreply.github.com>
2024-11-25 11:36:58 +0530

    Successfully scraped amazon mobile phone url using scrapy-playwright

--- ecomscraper/ecomscraper/spiders/amazon_spider.py
+++ ecomscraper/ecomscraper/spiders/amazon_spider.py
@@ -1,4 +1,9 @@
+from typing import Iterable
+
 import scrapy
+from scrapy import Request
+
+AMAZON_MOBILE_PHONES_URL = "https://www.amazon.in/s?i=electronics&rh=n%3A1389432031&s=popularity-rank&fs=true&ref=lp_1389432031_sar"
 
 
 class AmazonSpiderSpider(scrapy.Spider):
@@ -6,5 +11,13 @@ class AmazonSpiderSpider(scrapy.Spider):
     allowed_domains = ["www.amazon.in"]
     start_urls = ["https://www.amazon.in/"]
 
+    def start_requests(self) -> Iterable[Request]:
+        yield scrapy.Request(
+            url=AMAZON_MOBILE_PHONES_URL,
+            callback=self.parse,
+            meta={"playwright": True},
+
+        )
+
     def parse(self, response, **kwargs):
-        pass
+        yield {"url": response.url}
